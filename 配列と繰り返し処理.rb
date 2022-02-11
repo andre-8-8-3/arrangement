d827ff5c1ae29f0259a2db983f5a27bec4667d22
andre-8-8-3 <hhharleyyy883@gmail.com>
2022-02-11 20:49:33 +0900

    first commit

---
+++ 配列と繰り返し処理.rb
@@ -0,0 +1,5 @@
+users = ["satou", "tanaka", "yamada", "shimizu"]
+
+users.each do |name|
+    puts "hello!! #{name}"
+end
---
+++ 配列の定義.rb
@@ -0,0 +1,11 @@
+users = ["saitou", "tanaka", "suzuki", "yamada"]
+
+ages = [22, 34, 12, 45]
+
+
+puts ages [0] #22
+puts ages [1] #34
+puts ages [2] #12
+puts ages [3] #45
+
+puts users [2]
